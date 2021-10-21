8aa1544bb3187c6b0ef5c230ba575605aead1f4f
Marvin Qian <marvinqian@microsoft.com>
2021-10-21 15:31:34 +0800

    add pretty to Org

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(epg-gpg-program "/usr/bin/gpg")
- '(org-agenda-files
-   '("~/Org/GTD/Work.org" "~/Org/GTD/Projects.org" "~/Org/GTD/Habit.org" "~/Org/GTD/Event.org" "~/Org/GTD/Agenda.org"))
+ '(epg-gpg-program "C:/Program Files (x86)/GnuPG/bin/gpg")
+ '(org-agenda-files '("c:/Users/marvinqian/Org/GTD/Agenda.org"))
  '(org-download-image-dir "./IMG" t)
  '(safe-local-variable-values '((org-download-image-dir . "./IMG")))
  '(send-mail-function 'mailclient-send-it))
